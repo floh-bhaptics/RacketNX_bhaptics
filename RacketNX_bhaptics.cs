ea1ade7ee2b151490e03d178d9b9889821c9212e
floh-bhaptics <floh@fahrenbergers.de>
2022-02-09 22:23:55 +0100

    Projektdateien hinzufügen.

---
+++ RacketNX_bhaptics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+
+namespace RacketNX_bhaptics
+{
+    public class RacketNX_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        /*
+        [HarmonyPatch(typeof(VertigoPlayer), "Die", new Type[] { })]
+        public class bhaptics_PlayerDies
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StopThreads();
+            }
+        }
+        */
+    }
+}
